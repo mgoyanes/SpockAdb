ef744d023770fb03b3ca704e831b999a70ec5439
Miguel Goyanes <miguel.machado@wit-software.com>
2021-04-24 18:57:12 +0100

    Adds support for getting the backstack activities in Android 11

--- src/main/kotlin/spock/adb/command/GetBackStackCommand.kt
+++ src/main/kotlin/spock/adb/command/GetBackStackCommand.kt
@@ -2,29 +2,48 @@ package spock.adb.command
 
 import com.android.ddmlib.IDevice
 import com.intellij.openapi.project.Project
+import spock.adb.ShellOutputReceiver
+import spock.adb.getApiVersion
 import spock.adb.models.ActivityData
 import java.util.concurrent.TimeUnit
-import spock.adb.ShellOutputReceiver
 
 class GetBackStackCommand : Command<Any, List<ActivityData>> {
 
     companion object {
         const val ACTIVITY_DELIMITER = "Running activities (most recent first):"
         const val ACTIVITY_PREFIX = "Run #"
+        const val HIST_PREFIX = "* Hist"
         const val ACTIVITY_PREFIX_DELIMITER = "."
-        val extractAppRegex = Regex("(A=|I=)([a-zA-Z.]+)")
+        const val API_VERSION_11 = 11
+        const val EMPTY = ""
+        val extractAppRegex = Regex("(A=|I=|u0\\s)([a-zA-Z.]+)")
         val extractActivityRegex = Regex("(u0\\s[a-zA-Z.]+/)([a-zA-Z.]+)")
     }
 
     override fun execute(p: Any, project: Project, device: IDevice): List<ActivityData> {
         val shellOutputReceiver = ShellOutputReceiver()
-        device.executeShellCommand(
-            "dumpsys activity activities | sed -En -e '/Running activities/,/Run #0/p'",
-            shellOutputReceiver,
-            15L,
-            TimeUnit.SECONDS
-        )
-        return getCurrentRunningActivities(shellOutputReceiver.toString())
+        val apiVersion = device.getApiVersion()
+
+        return when {
+            apiVersion == null || apiVersion < API_VERSION_11 -> {
+                device.executeShellCommand(
+                    "dumpsys activity activities | sed -En -e '/Running activities/,/Run #0/p'",
+                    shellOutputReceiver,
+                    15L,
+                    TimeUnit.SECONDS
+                )
+                getCurrentRunningActivities(shellOutputReceiver.toString())
+            }
+            else -> {
+                device.executeShellCommand(
+                    "dumpsys activity activities | grep Hist",
+                    shellOutputReceiver,
+                    15L,
+                    TimeUnit.SECONDS
+                )
+                getCurrentRunningActivitiesAboveApi11(shellOutputReceiver.toString())
+            }
+        }
     }
 
     private fun getCurrentRunningActivities(bulkActivitiesData: String): List<ActivityData> {
@@ -56,5 +75,29 @@ class GetBackStackCommand : Command<Any, List<ActivityData>> {
                         }
                 )
             }
+    }
+
+    private fun getCurrentRunningActivitiesAboveApi11(bulkActivitiesData: String): List<ActivityData> {
+        lateinit var appPackage: String
+
+        return bulkActivitiesData
+            .lines()
+            .filter { it.trim().startsWith(HIST_PREFIX) }
+            .groupBy(
+                keySelector = {
+                    appPackage = extractAppRegex.find(it)?.groups?.lastOrNull()?.value ?: EMPTY
+                    appPackage
+                },
+                valueTransform = { bulkActivityData ->
+                    extractActivityRegex.find(bulkActivityData)?.groups?.lastOrNull()?.value?.let { activityName ->
+                        when {
+                            activityName.startsWith(ACTIVITY_PREFIX_DELIMITER) -> "$appPackage$activityName"
+                            else -> activityName
+                        }
+                    } ?: EMPTY
+                }
+            )
+            .filter { it.key.isNotBlank() }
+            .map { activityData -> ActivityData(activityData.key, activityData.value) }
     }
 }
